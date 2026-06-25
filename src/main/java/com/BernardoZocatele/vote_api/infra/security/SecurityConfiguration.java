a95f6e91d722c291ea40c86ca50796bf207c66e3
BernardoZocatele <bernardonovaeszocatele@gmail.com>
2026-06-25 16:26:40 -0300

    fix: add missing security configuration content

---
+++ src/main/java/com/BernardoZocatele/vote_api/infra/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.BernardoZocatele.vote_api.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                           .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                           .authorizeHttpRequests(authorize -> authorize
+                                .anyRequest().permitAll()
+                           )
+                           .build();
+    }
+}
