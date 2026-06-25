0f366ecbdaf8f1501371bb718e0713cb35850042
BernardoZocatele <bernardonovaeszocatele@gmail.com>
2026-06-25 15:14:50 -0300

    feat: initial spring boot project setup

---
+++ src/test/java/com/BernardoZocatele/vote_api/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BernardoZocatele.vote_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
