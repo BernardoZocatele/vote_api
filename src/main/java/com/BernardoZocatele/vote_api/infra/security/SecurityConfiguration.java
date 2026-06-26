3cac3f2746121f0fab8a68dfb88d0a60abe7c500
BernardoZocatele <bernardonovaeszocatele@gmail.com>
2026-06-26 13:44:47 -0300

    fix: add @Bean on SecurityFilterChain to fix 401 error and create AuthenticationManager and PasswordEncoder

--- src/main/java/com/BernardoZocatele/vote_api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/BernardoZocatele/vote_api/infra/security/SecurityConfiguration.java
@@ -1,15 +1,21 @@
 package com.BernardoZocatele.vote_api.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity.csrf(csrf -> csrf.disable())
                            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -17,5 +23,15 @@ public class SecurityConfiguration {
                                 .anyRequest().permitAll()
                            )
                            .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
